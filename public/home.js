0a9a4e3641e9e4949225581128e365f466ef5ad1
unknown <thesoupissoup@gmail.com>
2018-09-07 22:53:53 -0700

    data displayed with Url and text area for comments.

--- public/home.js
+++ public/home.js
@@ -14,15 +14,26 @@ $("#art").on("click", function(){
 
 
  $.get("/articles", function(data){
-      console.log(data)  
-     });
-
-
-
+      console.log(data) 
+      
+            for (var i = 0; i < data.length; i++) {
+                // Display the apropos information on the page
+                $("#data").append("<div class = card>"+"<h2>"+data[i].Headline+"</h2>")
+                
+                $("#data").append( "<a href =" + data[i].URL+ " >"+ "Go to the Article" + "</a>"+
+                "<a class=btn btn-primary href=note.html role=button>"+"Link"+"</a>"+"</div>")
+                
+                $("#data").append("<form>"+"<div class=form-group>"+
+                "<label for=exampleFormControlTextarea1>"+"Example textarea"+"</label>"+
+                "<textarea class=form-control id=exampleFormControlTextarea1 rows=3>"+"</textarea>"+
+              "</div>"+
+            "</form>");
+            } 
+        });
+        
+          // <p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+          
+      
+        });
 
  });
-
-
-
-
-});
